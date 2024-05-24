19e850011cf7351364bc5fa5d6c902ecfb7060fc
Iida Lassila <iida.m.lassila@helsinki.fi>
2024-05-24 12:18:58 +0300

    Add distance_metrics, modify MNISTLoader

--- src/data_loader.py
+++ src/data_loader.py
@@ -9,8 +9,19 @@ class MNISTLoader:
         
     def load_data(self):
         mnist = fetch_openml('mnist_784', version=1)
-        self.data = mnist['data']
-        self.target = mnist['target']
 
+        self.data = mnist['data'].reshape(-1, 28, 28)
+        self.target = mnist['target'].astype(np.uint8)
+
+        self.data = (self.data > self.threshold_value).astype(np.uint8)
 
         return self.data, self.target
+    
+    def split_data(self):
+        X_train = self.data[10:20]
+        X_test = self.data[60000:60010]
+        y_train = self.target[10:20]
+        y_test = self.target[60000:60010]
+
+        return X_train, X_test, y_train, y_test
+    
--- src/ui/start_view.py
+++ src/ui/start_view.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 from tkinter import ttk, constants
-from PIL import Image, ImageTk
 import numpy as np
 import random
+from PIL import Image, ImageTk
 from data_loader import MNISTLoader
+from distance_metrics import undirected_hausdorff, directed_hausdorff
 
 class StartView:
     def __init__(self, root):
@@ -12,29 +13,53 @@ class StartView:
 
         self.loader = MNISTLoader()
         self.data, self.target = self.loader.load_data()
+        self.X_train, self.X_test, self.y_train, self.y_test = self.loader.split_data()
+
+        self.labels = []
+        self.distances = []
 
         self._initialize()
-        self._show_random_image() 
+        self._show_test_number_and_distances()
 
     def pack(self):
         self._frame.pack(fill=constants.BOTH, expand=True)
 
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
-        self.label = ttk.Label(master=self._frame, text="Satunnainen numero MNIST-datasta")
-        self.label.grid(row=0, column=0, padx=10, pady=10)
 
-        self.image_label = tk.Label(master=self._frame)
-        self.image_label.grid(row=1, column=0, padx=10, pady=10)
+        for i in range(len(self.X_train) + 1):
+            label = ttk.Label(master=self._frame)
+            label.grid(row=i, column=0, padx=10, pady=10)
+            self.labels.append(label)
 
-        self.random_button = ttk.Button(master=self._frame, text="Näytä uusi numero", command=self._show_random_image)
-        self.random_button.grid(row=2, column=0, padx=10, pady=10)
+            distance = ttk.Label(master=self._frame)
+            distance.grid(row=i, column=1, padx=10, pady=10)
+            self.distances.append(distance)
 
-    def _show_random_image(self):
-        self.current_image_index = random.randint(0, len(self.data) - 1)
-        image_data = self.data[self.current_image_index].reshape(28, 28).astype(np.uint8)
+    def _show_test_number_and_distances(self):
 
-        img = Image.fromarray(image_data)
-        imgtk = ImageTk.PhotoImage(image=img)
-        self.image_label.config(image=imgtk)
-        self.image_label.image = imgtk 
+        single_image_matrix = self.X_test[0]
+        single_image_label = self.y_test[0]
+
+        print(single_image_matrix)
+
+        single_image_points = [(i, j) for i in range(single_image_matrix.shape[0]) for j in range(single_image_matrix.shape[1]) if single_image_matrix[i, j] == 1]
+
+        print(single_image_points)
+
+        distances = []
+        
+        for train_image_vector in self.X_train:
+            train_image_points = [(i, j) for i in range(train_image_vector.shape[0]) for j in range(train_image_vector.shape[1]) if train_image_vector[i, j] == 1]
+            distances.append(directed_hausdorff(train_image_points, single_image_points, 'd6'))
+
+
+        self.labels[0].config(text=f"Testinumero: {single_image_label}")
+
+        for i in range(len(self.X_train)):
+            self.labels[i + 1].config(text=self.y_train[i])
+            self.distances[i + 1].config(text=f"Etäisyys: {distances[i]}")
+
+        print(f"Vertailunumerot: {self.y_train}")
+
+        print(f"Etäisyydet: {distances}")
