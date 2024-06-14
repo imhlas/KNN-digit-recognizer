9174ec3739f001c0fb6eebeef54b1c4b9b677bbd
Iida Lassila <iida.m.lassila@helsinki.fi>
2024-06-14 11:50:51 +0300

    Add coverage & invoke

---
+++ tasks.py
@@ -0,0 +1,15 @@
+from invoke import task
+
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
+def coverage_report(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+    ctx.run("coverage report -m", pty=True)
+    ctx.run("coverage html", pty=True)
